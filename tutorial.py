a33ce5ef3c9d61f116d734de28906bc9132cb86b
Emily Webber <egwebber@cs.uchicago.edu>
2017-04-15 17:15:35 -0500

    Adding next rasterized maps

--- tutorial.py
+++ tutorial.py
@@ -4,6 +4,9 @@ from matplotlib import pyplot as plt
 from shapely.geometry import Point, LineString, Polygon, MultiPoint, GeometryCollection
 import rasterio
 import cartopy.crs as ccrs
+from rasterio.features import rasterize
+from geopandas import read_file
+
 
 def print_a_few_lines(input_file):
 	with fiona.open(input_file) as src:
@@ -44,8 +47,36 @@ def rasterio_exercise():
 		plt.savefig("rasterio_cartopy.png", dpi=300)
 		plt.show()
 
+def show_img(img, bounds):
+	left, bottom, right, top = bounds
+	plt.imshow(img, cmap="gray", extent=(left, right, bottom, top))
+	locs, labels = plt.xticks()
+	plt.setp(labels, rotation=90)
 
-
+def rasterio_mask():
+	vector_file = "/Users/Emily/Desktop/SciPy-Tutorial-2015/examples/nybb_15b/nybb.shp"
+	raster_file = "/Users/Emily/Desktop/SciPy-Tutorial-2015/data/manhattan.tif"
+	df = read_file(vector_file)
+	plt.figure(1)
+	with rasterio.open(raster_file) as src:
+		poly = df.to_crs(src.crs).ix[3]["geometry"]
+		coords = [p.exterior.coords.xy for p in poly]
+		ax = plt.subplot(1,2,1)
+		img = src.read(1)
+		show_img(src.read(1), src.bounds)
+		for x, y in coords:
+			plt.plot(x, y, color="red", linewidth=2)
+		ax.set_xlim(src.bounds.left, src.bounds.right)
+		ax.set_ylim(src.bounds.bottom, src.bounds.top)
+		plt.subplot(1,2,2)
+		mask = rasterize([poly], transform=src.transform, out_shape=src.shape)
+		img[mask==0] = 225
+		show_img(img, src.bounds)
+	plt.savefig("rasterize_mask.png", dpi=300)
+	plt.figure(2)
+	show_img(mask, src.bounds)
+	plt.savefig("mask.png", dpi=300)
+	plt.show()
 
 if __name__ == "__main__":
 	input_file = "/Users/Emily/Desktop/SciPy-Tutorial-2015/examples/nybb_15b/nybb.shp"
@@ -56,4 +87,6 @@ if __name__ == "__main__":
 
 	# shapely_exercise()
 
-	rasterio_exercise()
+	# rasterio_exercise()
+
+	rasterio_mask()
